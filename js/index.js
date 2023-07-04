39100a2dbf2f69add51c0455d6ef2e4a67895d1a
AmirAnsarpour <amir.ansarpour2003@gmail.com>
2023-07-04 21:52:24 +0330

    .

--- js/index.js
+++ js/index.js
@@ -6,9 +6,7 @@ function addCourse() {
     let grade = parseInt(document.getElementById("grade").value);
     let credit = parseInt(document.getElementById("credit").value);
 
-    // Check if inputs are valid
     if (name && grade && credit && grade <= 20 && credit <= 4) {
-        // Add course to list
         coursesList.push({
             name: name,
             grade: grade,
@@ -27,12 +25,10 @@ function addCourse() {
 function showCourses() {
     let table = document.getElementById("courses");
 
-    // Clear old rows
     while (table.rows.length > 1) {
         table.deleteRow(1);
     }
 
-    // Insert new rows
     for (let i = 0; i < coursesList.length; i++) {
         let row = table.insertRow(i + 1);
         row.insertCell(0).innerHTML = i + 1;
@@ -40,9 +36,10 @@ function showCourses() {
         row.insertCell(2).innerHTML = coursesList[i].grade;
         row.insertCell(3).innerHTML = coursesList[i].credit;
 
-        // Add background color to cells with grade less than 10
         if (coursesList[i].grade < 10) {
-            row.cells[2].style.backgroundColor = "red";
+            row.cells[2].style.backgroundColor = "rgba(255, 0, 0, 0.2)";
+        } else {
+            row.cells[2].style.backgroundColor = "rgba(0, 255, 0, 0.2)";
         }
     }
 }
@@ -52,7 +49,6 @@ function calculateAverage() {
     let totalGrade = 0;
 
     for (let i = 0; i < coursesList.length; i++) {
-        // Check if grade and credit are valid
         if (coursesList[i].grade >= 10 && coursesList[i].grade <= 20 && coursesList[i].credit <= 4) {
             totalCredit += coursesList[i].credit;
             totalGrade += coursesList[i].grade * coursesList[i].credit;
@@ -66,7 +62,7 @@ function calculateAverage() {
         if (average >= 12) {
             averageElement.style.color = "green";
         } else {
-            averageElement.style.color = "red";
+            averageElement.style.color = "rgba(255, 0, 0, 0.8)";
         }
 
         averageElement.innerHTML = average.toFixed(2);
